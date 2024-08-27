024d2a00c8856a8c15d8cc280994c189d5b5d6a3
Moustapha-S6 <msy43467@gmail.com>
2024-08-27 14:52:13 +0000

    initial commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,117 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+<!doctype html>
+<html lang="en" class="light-theme">
 
-        <x-validation-errors class="mb-4" />
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        @session('status')
+  <!-- loader-->
+  <link href="{{asset('Back/assets/css/pace.min.css')}}" rel="stylesheet" />
+  <script src="{{asset('Back/assets/js/pace.min.js')}}"></script>
+
+  <!--plugins-->
+  <link href="{{asset('Back/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css')}}" rel="stylesheet" />
+
+  <!-- CSS Files -->
+  <link href="{{asset('Back/assets/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('Back/assets/css/bootstrap-extended.css')}}" rel="stylesheet">
+  <link href="{{asset('Back/assets/css/style.css')}}" rel="stylesheet">
+  <link href="{{asset('Back/assets/css/icons.css')}}" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+
+  <title>Blackdash - Bootstrap5 Admin Template</title>
+</head>
+
+<body>
+
+  <!--start wrapper-->
+  <div class="wrapper">
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-transparent p-3">
+        <div class="container-fluid">
+          <a href="javascript:;"><img src="{{asset('Back/assets/images/logo-icon-3.png')}}" width="140" alt="" /></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
+    </header>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-4 col-lg-5 col-md-7 mx-auto mt-5">
+          <div class="card radius-10">
+            <div class="card-body p-4">
+              <div class="text-center">
+                <h4>Sign In</h4>
+                <p>Sign In to your account</p>
+              </div>
+                @session('status')
             <div class="mb-4 font-medium text-sm text-green-600">
                 {{ $value }}
             </div>
-        @endsession
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+           @endsession
+              <form method="POST" action="{{ route('login') }}" class="form-body row g-3">
+                @csrf
+                <div class="col-12">
+                  <label for="inputEmail" class="form-label">Email</label>
+                  <input type="email" class="form-control" id="inputEmail" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                </div>
+                <div class="col-12">
+                  <label for="inputPassword" class="form-label">Password</label>
+                  <input type="password" class="form-control"id="password" name="password" required autocomplete="current-password" />
+                </div>
+                <div class="col-12 col-lg-6">
+                  <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckRemember">
+                    <label class="form-check-label" for="flexSwitchCheckRemember">Remember Me</label>
+                  </div>
+                </div>
+                <div class="col-12 col-lg-6 text-end">
+                  <a href="authentication-reset-password-simple.html">Forgot Password?</a>
+                </div>
+                <div class="col-12 col-lg-12">
+                  <div class="d-grid">
+                    <button type="submit" class="btn btn-dark">Sign In</button>
+                  </div>
+                </div>
+                <div class="col-12 col-lg-12">
+                  <div class="position-relative border-bottom my-3">
+                    <div class="position-absolute seperator translate-middle-y">or continue with</div>
+                  </div>
+                </div>
+                <div class="col-12 col-lg-12">
+                  <div class="social-login d-flex flex-row align-items-center justify-content-center gap-2 my-2">
+                    <a href="javascript:;" class=""><img src="{{asset('Back/assets/images/icons/facebook.png')}}" alt=""></a>
+                    <a href="javascript:;" class=""><img src="{{asset('Back/assets/images/icons/apple-black-logo.png')}}" alt=""></a>
+                    <a href="javascript:;" class=""><img src="{{asset('Back/assets/images/icons/google.png')}}" alt=""></a>
+                  </div>
+                </div>
+                <div class="col-12 col-lg-12 text-center">
+                  <p class="mb-0">Don't have an account? <a href="authentication-sign-up-simple.html">Sign up</a></p>
+                </div>
+              </form>
             </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="my-5">
+      <div class="container">
+        <div class="text-center">
+          <p class="mb-0">Copyright © 2021 UI Admin by Codervent.</p>
+        </div>
+      </div>
+    </footer>
+  </div>
+  <!--end wrapper-->
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+</body>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+</html>
 
-                <x-button class="ms-4">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,60 +1,127 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
 
-        <x-validation-errors class="mb-4" />
+<!doctype html>
+<html lang="en" class="light-theme">
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-            <div>
-                <x-label for="name" value="{{ __('Name') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            </div>
+  <!-- loader-->
+  <link href="{{asset('Back/assets/css/pace.min.css')}}" rel="stylesheet" />
+  <script src="{{asset('Back/assets/js/pace.min.js')}}"></script>
 
-            <div class="mt-4">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            </div>
+  <!--plugins-->
+  <link href="{{asset('Back/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css')}}" rel="stylesheet" />
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
+  <!-- CSS Files -->
+  <link href="{{asset('Back/assets/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('Back/assets/css/bootstrap-extended.css')}}" rel="stylesheet">
+  <link href="{{asset('Back/assets/css/style.css')}}" rel="stylesheet">
+  <link href="{{asset('Back/assets/css/icons.css')}}" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
 
-            <div class="mt-4">
-                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
+  <title>Blackdash - Bootstrap5 Admin Template</title>
+</head>
 
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="mt-4">
-                    <x-label for="terms">
-                        <div class="flex items-center">
-                            <x-checkbox name="terms" id="terms" required />
+<body>
 
-                            <div class="ms-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Privacy Policy').'</a>',
-                                ]) !!}
-                            </div>
-                        </div>
-                    </x-label>
+  <!--start wrapper-->
+  <div class="wrapper">
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-transparent p-3">
+        <div class="container-fluid">
+          <a href="javascript:;"><img src="{{asset('Back/assets/images/logo-icon-3.png')}}" width="140" alt="" /></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        
+        </div>
+      </nav>
+    </header>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-5 col-lg-6 col-md-7 mx-auto mt-5">
+          <div class="card radius-10">
+            <div class="card-body p-4">
+              <div class="text-center">
+                <h4>Sign Up</h4>
+                <p>Creat New account</p>
+              </div>
+                 @session('status')
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ $value }}
+             </div>
+             @endsession
+              <form class="form-body row g-3" method="POST" action="{{ route('register') }}">
+                @csrf
+                <div class="col-12">
+                  <label for="inputName" class="form-label">Name</label>
+                  <input type="text" class="form-control" id="inputName" name="name" :value="old('name')" required autofocus autocomplete="name" />
                 </div>
-            @endif
+                <div class="col-12">
+                  <label for="inputEmail" class="form-label">Email</label>
+                  <input type="email" class="form-control" id="email"  name="email" :value="old('email')" required autocomplete="username">
+                </div>
+                <div class="col-12">
+                  <label for="inputPassword" class="form-label">Password</label>
+                  <input type="password" class="form-control"  id="password" name="password" required autocomplete="new-password">
+                </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ms-4">
-                    {{ __('Register') }}
-                </x-button>
+                 <div class="col-12">
+                  <label for="inputPassword" class="form-label"> Confirm Password</label>
+                  <input id="password_confirmation" class="form-control"type="password" name="password_confirmation" required autocomplete="new-password" />
+                </div>
+                <div class="col-12 col-lg-12">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                    <label class="form-check-label" for="flexCheckChecked">
+                      I agree the Terms and Conditions
+                    </label>
+                  </div>
+                </div>
+                <div class="col-12 col-lg-12">
+                  <div class="d-grid">
+                    <button type="submit" class="btn btn-dark">Sign Up</button>
+                  </div>
+                </div>
+                <div class="col-12 col-lg-12">
+                  <div class="position-relative border-bottom my-3">
+                    <div class="position-absolute seperator translate-middle-y">or continue with</div>
+                  </div>
+                </div>
+                <div class="col-12 col-lg-12">
+                  <div class="social-login d-flex flex-row align-items-center justify-content-center gap-2 my-2">
+                    <a href="javascript:;" class=""><img src="{{asset('Back/assets/images/icons/facebook.png')}}" alt=""></a>
+                    <a href="javascript:;" class=""><img src="{{asset('Back/assets/images/icons/apple-black-logo.png')}}" alt=""></a>
+                    <a href="javascript:;" class=""><img src="{{asset('Back/assets/images/icons/google.png')}}" alt=""></a>
+                  </div>
+                </div>
+                <div class="col-12 col-lg-12 text-center">
+                  <p class="mb-0">Already have an account? <a href="authentication-sign-in-simple.html">Sign in</a></p>
+                </div>
+              </form>
             </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="my-5">
+      <div class="container">
+        <div class="text-center">
+          <p class="mb-0">Copyright © 2024 UI Admin by Codervent.</p>
+        </div>
+      </div>
+    </footer>
+  </div>
+  <!--end wrapper-->
+
+
+</body>
+
+</html>
+
+
